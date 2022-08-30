3e6125d0de61b9bf6c814f4d31e37059bea3d3f2
vskidamian <jakubowski1337@gmail.com>
2022-08-30 22:09:40 +0200

    feat: light dark switch

--- components/ThemeSwitch/index.tsx
+++ components/ThemeSwitch/index.tsx
@@ -3,6 +3,8 @@ import { Switch } from '@headlessui/react'
 import { useState, useEffect } from 'react'
 import { useTheme } from 'next-themes'
 
+import { SunIcon, MoonIcon } from '@heroicons/react/solid'
+
 type ContentProps = {
     
 }
@@ -32,9 +34,11 @@ export const Content = () => {
             className={`${checked ? 'bg-primaryDark' : 'bg-primary'} relative h-6 w-11 items-center rounded-full hidden md:inline-flex`}
         >
             <span
-                className={`${checked ? 'translate-x-6' : 'translate-x-1'
-                    } inline-block h-4 w-4 rounded-full bg-white transform transition ease-in-out duration-200`}
-            />
+                className={`${checked ? 'translate-x-5' : 'translate-x-1'
+                    } inline-block transform transition ease-in-out duration-200 h-5 w-5 rounded-full relative`}>
+                    <SunIcon className={`h-5 w-5 text-white transform transition ease-in-out duration-300 absolute  ${checked ? 'opacity-100' : 'opacity-0'}`} />
+                    <MoonIcon className={`h-5 w-5 text-white transform transition ease-in-out duration-300 absolute ${checked ? 'opacity-0' : 'opacity-100'}`} />
+            </span>
         </Switch>
     )
 }
